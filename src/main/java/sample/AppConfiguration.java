c5f5b7ccd242ec38e3ffbd92a54ade483e5f9518
niaoshuai <niao.shuai123@163.com>
2020-01-11 09:45:45 +0800

    增加POM

--- src/main/java/sample/AppConfiguration.java
+++ src/main/java/sample/AppConfiguration.java
@@ -5,13 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import static sample.SpringExtension.SpringExtProvider;
 
 /**
  * The application configuration.
  */
 @Configuration
-class AppConfiguration {
+public class AppConfiguration {
 
   // the application context is needed to initialize the Akka Spring Extension
   @Autowired
